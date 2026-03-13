26520ba5a278e18ac5d461a689ba65b85e91ea90
JonasFilhoDev <jonasfilho1985@gmail.com>
2026-03-13 16:56:45 -0300

    correçoes no frontend

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js'
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                }
+            },
+            globals: {
+                window: 'readonly',
+                document: 'readonly',
+                console: 'readonly',
+                process: 'readonly'
+            }
+        },
+        rules: {
+            'no-unused-vars': 'warn'
+        }
+    }
+]
