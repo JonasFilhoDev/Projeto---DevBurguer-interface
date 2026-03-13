90ecaadff8d1b6f6a1409b9874e6130d49cf4a48
JonasFilhoDev <jonasfilho1985@gmail.com>
2026-03-13 15:08:07 -0300

    correção baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://projeto-dev-burguer-interface.vercel.app/',
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001',
 });
 
 
